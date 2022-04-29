bd25e16d532e3ba36d5625498bcb464ff03cd588
Joseph Darville <joseph.darville9@gmail.com>
2022-04-29 19:26:04 -0400

    added conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,57 @@
-var playerName = window.prompt("What is your robot's name?")
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
 
-console.log (playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAtack  = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName)
+var fight = function () {
+    // Alert players that they are starting the round
+    window.alert("welcome to Robot Gladiators!");
 
-function fight() {
-    window.alert("The fight has begun!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    
+    playerHealth = playerHealth - enemyAtack;
+    
+    console.log (
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if(confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight!");
+            playerMoney = playerMoney - 2;
+        }
+        else {
+            fight();
+        }
+    } else {
+        window.alert("You need to hoose a valid option. Try Again!");
+    }
 }
 
-// fight();
+    fight();
